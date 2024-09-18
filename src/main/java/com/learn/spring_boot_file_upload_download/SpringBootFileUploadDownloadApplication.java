d2bfc1b2807067bf6c9548d7af061f705a596da7
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-09-18 20:05:44 +0530

    first

---
+++ src/main/java/com/learn/spring_boot_file_upload_download/SpringBootFileUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring_boot_file_upload_download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileUploadDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileUploadDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/spring_boot_file_upload_download/SpringBootFileUploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.spring_boot_file_upload_download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFileUploadDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
